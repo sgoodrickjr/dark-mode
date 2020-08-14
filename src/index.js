97de68e054cdd7d4ac1562769c364e7f2071db5c
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-08-13 21:16:37 -0600

    functional programming template for storing values in localStorage

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [localData, setLocalData] = useLocalStorage();
 
   useEffect(() => {
     axios
